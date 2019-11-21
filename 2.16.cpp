c9e97f047a676af1c7d95e58aa9d75156f50472f
NoorFatima0151 <57993812+NoorFatima0151@users.noreply.github.com>
2019-11-21 06:05:28 +0500

    Add files via upload

---
+++ 2.16.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x,y;
+	cout<<"enter value of x: ";
+	cin>>x;
+	cout<<"enter value of y: ";
+	cin>>y;
+	//output
+	cout<<"the sum of x and y is: "<<x+y<<endl;
+	cout<<"the product is: "<<x*y<<endl;
+	cout<<"the difference of x and y: "<<x-y<<endl;
+	cout<<"the quotient is: "<<x/y<<endl;
+	 return 0;
+}
---
+++ 2.17.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main ()
+ {
+ 	// a)
+ 	cout<<"welcome to"<<" c++ programming"<<endl;
+ 	
+ 	// b)
+ 	cout<<"welcome"<<"to "<<"c++"<<" programming"<<endl;
+ 	
+ 	// c)
+ 	cout<<"welcome to c++ programming";
+ 	
+	return 0;
+}
